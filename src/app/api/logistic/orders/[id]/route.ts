36e75748ea1a6b8c696c76d3c152b1bf788faa77
Elgin Brian Wahyu Bramadhika <132267129+elginbrian@users.noreply.github.com>
2025-10-03 23:49:53 +0700

    fix(vercel): fixing vercel error

--- src/app/api/logistic/orders/[id]/route.ts
+++ src/app/api/logistic/orders/[id]/route.ts
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { createAdminClient, createLegacyServerClient } from "@/lib/supabase";
 
-export async function GET(req: NextRequest, { params }: { params: { id: string } }) {
+export async function GET(req: NextRequest, context: any) {
+  const params = await context.params;
   try {
     let supabase;
     try {
